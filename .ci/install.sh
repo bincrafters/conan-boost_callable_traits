81b6bc8a476fffdd1a0e01a969e7df86ddd60b22
Jerry W <jerrywiltse@gmail.com>
2019-01-22 22:58:38 -0500

    standardize CI files for travis

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,6 +3,23 @@
 set -e
 set -x
 
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    brew update || brew update
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+    brew install cmake || true
+
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+
+    pyenv install 3.6.6
+    pyenv virtualenv 3.6.6 conan
+    pyenv rehash
+    pyenv activate conan
+fi
+
 pip install conan --upgrade
 pip install conan_package_tools bincrafters_package_tools
+
 conan user
