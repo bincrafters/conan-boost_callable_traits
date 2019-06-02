342be3cfb0601a1fb142aed793fb224e7bf95795
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:24 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,9 @@
 from conans import python_requires
 import os
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostCallable_TraitsConan(base.BoostBaseConan):
     name = "boost_callable_traits"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_callable_traits"
-    lib_short_names = ["callable_traits"]
-    header_only_libs = ["callable_traits"]
+    version = "1.70.0"
